3d500c46f444393d3850e33a640b65c9e134fd59
Awsomv30 <44628705+Awsomv30@users.noreply.github.com>
2018-11-13 05:07:08 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -49,4 +49,16 @@ msg.delete();
 })
 }
 });
+
+client.on('ready', () => {
+   console.log(`----------------`);
+      console.log(`Desert Bot- Script By : EX Clan`);
+        console.log(`----------------`);
+      console.log(`ON ${client.guilds.size} Servers '     Script By : EX Clan ' `);
+    console.log(`----------------`);
+  console.log(`Logged in as ${client.user.tag}!`);
+client.user.setGame(`3rab-pros|server`,"http://twitch.tv/3rab-pros")
+client.user.setStatus("dnd")
+});
+
 client.login(process.env.BOT_TOKEN);
